a90c722795395d0b899128311da0a8629ec13d89
AndyTex2003 <andersonrexbatista@gmail.com>
2025-09-02 23:11:00 -0300

    Adicionei testes automatizados da função somar dois números

--- tests/calculadora.test.js
+++
@@ -1,5 +0,0 @@
-const { somarDoisNumeros } = require('../src/calculadora'); // comando para importar a função somarDoisNumeros
-
-const resultadoDaSoma = somarDoisNumeros(5, 3);
-
-console.log(resultadoDaSoma);
